ba2795f5409eba7ca5b675c3e6d683eda034304a
Padmaksh Mishra <padmakshmishra@gmail.com>
2022-07-27 19:21:41 +0530

    Quick Sort added

--- sorting.c
+++ sorting.c
@@ -1,10 +1,33 @@
 # include <stdio.h>
 
-void swap(int *a, int *b) {
+// Helping Functions
+void swap(int *a,int *b);
+int partition(int *arr,int lo, int hi);
+
+// Sorting Functions
+void insertion_sort(int arr[], int n);      // O(n^2)
+void quick_sort(int arr[], int low, int high);    // O(nlogn)
+
+
+int main(void){
+    int arr[] = {9,8,7,6,5,4,3,2,1};
+    for(int i=0;i<9;++i) printf("%d ", arr[i]);
+    printf("\n");
+    //insertion_sort(arr,9);
+    quick_sort(arr,0,8);
+    for(int i=0;i<9;++i) printf("%d ", arr[i]);
+
+    return 0;
+}
+
+void swap(int *a,int *b)
+{
     int tmp = *a;
     *a = *b;
     *b = tmp;
+    return;
 }
+
 void insertion_sort(int arr[], int n)
 {
     int i, key, j;
@@ -17,11 +40,31 @@ void insertion_sort(int arr[], int n)
         }
         arr[j + 1] = key;
     }
+    return;
 }
-int main(void){
-    int arr[] = {9,8,7,6,5,4,3,2,1};
-    for(int i=0;i<9;++i) printf("%d ", arr[i]);
-    printf("\n");
-    insertion_sort(arr,9);
-    for(int i=0;i<9;++i) printf("%d ", arr[i]);
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quick_sort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
 }
