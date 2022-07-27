bb2812774f959ad3f4db526b71662840676378fc
Padmaksh Mishra <padmakshmishra@gmail.com>
2022-07-27 19:33:18 +0530

    Mrge Sort added

--- sorting.c
+++ sorting.c
@@ -2,22 +2,19 @@
 
 // Helping Functions
 void swap(int *a,int *b);
+void merge(int arr[], int l, int m, int r);
 int partition(int *arr,int lo, int hi);
 
 // Sorting Functions
 void insertion_sort(int arr[], int n);              // O(n^2)
 void selection_sort(int arr[], int n);              // O(n^2)
 void bubbleSort(int arr[], int n);                  // O(n^2)
+void mergeSort(int arr[], int l, int r);            // O(nlogn)
 void quick_sort(int arr[], int low, int high);      // O(nlogn)
 
 
 int main(void){
-    int arr[] = {9,8,7,6,5,4,3,2,1};
-    for(int i=0;i<9;++i) printf("%d ", arr[i]);
-    printf("\n");
-    //insertion_sort(arr,9);
-    quick_sort(arr,0,8);
-    for(int i=0;i<9;++i) printf("%d ", arr[i]);
+    printf("Welcome to the sorts\n");
 
     return 0;
 }
@@ -69,6 +66,60 @@ void bubbleSort(int arr[], int n)
         for (j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1])
                 swap(&arr[j], &arr[j + 1]);
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = arr[l + i];
+    }
+    for (j = 0; j < n2; j++)
+    {
+        R[j] = arr[m + 1 + j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
 }
 
 int partition (int arr[], int low, int high)
